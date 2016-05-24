80e015f8edbcaa856db2387806a2e9f5938d4c16
Eresor <trepala.aleksander@gmail.com>
2016-05-25 00:08:13 +0200

    Fixed bug of bot not starting to play as first player.

--- SI_Projekt_Warcaby/Game.cs
+++ SI_Projekt_Warcaby/Game.cs
@@ -263,7 +263,7 @@ namespace Warcaby
             else
             {
                 players[0] = new AIPlayer(this, p1);
-                currentPlayer = 1;
+                //currentPlayer = 1;
                 // bot here
             }
         }
